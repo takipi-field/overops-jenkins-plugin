1e8f451648736fe052898eb71ef9a726fcc01f75
Chen Harel <chook.harel@gmail.com>
2018-09-18 13:19:00 +0300

    Using API Core from Maven, bumping to 1.0.0

--- src/main/java/com/takipi/common/api/data/metrics/Graph.java
+++
@@ -1,9 +0,0 @@
-package com.takipi.common.api.data.metrics;
-
-import java.util.List;
-
-public class Graph {
-	public String type;
-	public String id;
-	public List<GraphPoint> points;
-}
--- src/main/java/com/takipi/common/api/request/intf/ApiRequest.java
+++
@@ -1,7 +0,0 @@
-package com.takipi.common.api.request.intf;
-
-public interface ApiRequest {
-	public String urlPath();
-
-	public String contentType();
-}
